6a3cdcecc9646e30ad787b4e567f1e28940e75b0
Dominic Egginton <dominic.egginton@gmail.com>
2021-11-06 21:12:00 +0000

    add route for getting github status

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ import Router from '@koa/router';
 
 /* IMPORT ROUTERS */
 import repositories from './repositories';
+import status from './status';
 
 /* SETUP ROUTER */
 const router = new Router();
 router.prefix('/api');
+router.use(status.routes());
+router.use(status.allowedMethods());
 router.use(repositories.routes());
 router.use(repositories.allowedMethods());
 
--- src/services/github.ts
+++ src/services/github.ts
@@ -1,5 +1,14 @@
 /* IMPORT MODULES */
 import { Octokit } from '@octokit/rest';
+import { graphql } from '@octokit/graphql';
+import type { GraphQlQueryResponseData } from '@octokit/graphql';
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+    'user-agent': 'dominicegginton-dev-api',
+  },
+});
 
 /* OCTOKIT */
 const octokit = new Octokit({
@@ -12,9 +21,8 @@ const octokit = new Octokit({
  * @param {string} username username of user
  * @returns list of users public repositories
  */
-export default async function repositories(username: string) {
-  const response = await octokit.repos.listForUser({
-    username,
+export async function repositories() {
+  const response = await octokit.repos.listForAuthenticatedUser({
     type: 'all',
     sort: 'pushed',
     direction: 'desc',
@@ -22,4 +30,19 @@ export default async function repositories(username: string) {
     page: 1,
   });
   return response.data;
+}
+
+export async function status() {
+  const response: GraphQlQueryResponseData = await graphqlWithAuth(`
+    {
+      user(login: "dominicegginton") {
+        status {
+          emojiHTML
+          message
+        }
+      }
+    }
+  `);
+
+  return response.user.status;
 }
